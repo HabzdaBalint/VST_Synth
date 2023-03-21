7049b53bfb8383807be6d6172596aadc939c89c3
HabzdaBalint <126023437+HabzdaBalint@users.noreply.github.com>
2023-03-21 16:29:10 +0100

    -Started basic GUI implementation and structure

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,14 +15,21 @@ VST_SynthAudioProcessorEditor::VST_SynthAudioProcessorEditor (VST_SynthAudioProc
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    addAndMakeVisible(keyboardComponent);
+
+    addAndMakeVisible(tabbedComponent);
+
+    setSize (700, 500);
+    setResizable(false, false);
+
+    setLookAndFeel(&lnf);
 }
 
 VST_SynthAudioProcessorEditor::~VST_SynthAudioProcessorEditor()
 {
+    setLookAndFeel(nullptr);
 }
 
-//==============================================================================
 void VST_SynthAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
@@ -30,11 +37,28 @@ void VST_SynthAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    //g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void VST_SynthAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    keyboardComponent.setBounds(0, getBounds().getBottom() - 100, 700, 100);
+
+    tabbedComponent.setBounds(0, 0, 700, 400);
 }
+
+//===================================================================================================================
+
+VST_SynthTabbedComponent::VST_SynthTabbedComponent(VST_SynthAudioProcessor& p)
+    : audioProcessor (p),
+      TabbedComponent(juce::TabbedButtonBar::TabsAtTop)
+{
+    auto color = getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId);
+
+    addTab("Synth", color, audioProcessor.additiveSynth->createEditor(), false, 0);
+    addTab("FX", color, audioProcessor.fxChain->createEditor(), false, 1);
+}
+
+VST_SynthTabbedComponent::~VST_SynthTabbedComponent() {}
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,18 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+
+class VST_SynthTabbedComponent : public juce::TabbedComponent
+{
+public:
+    VST_SynthTabbedComponent(VST_SynthAudioProcessor&);
+    ~VST_SynthTabbedComponent() override;
+private:
+    VST_SynthAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(VST_SynthTabbedComponent)
+};
+
 //==============================================================================
 /**
 */
@@ -28,6 +40,12 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     VST_SynthAudioProcessor& audioProcessor;
+
+    VST_SynthTabbedComponent tabbedComponent { audioProcessor };
+
+    juce::MidiKeyboardState keyboardState;
+    juce::MidiKeyboardComponent keyboardComponent { keyboardState, juce::MidiKeyboardComponent::horizontalKeyboard };
+    juce::LookAndFeel_V4 lnf = juce::LookAndFeel_V4(juce::LookAndFeel_V4::getMidnightColourScheme());
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (VST_SynthAudioProcessorEditor)
 };
